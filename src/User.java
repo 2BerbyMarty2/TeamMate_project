2d0751445c86e05db3a5e08048ce92d34e11a7e2
Vindula Viduranga <158612900+2BerbyMarty2@users.noreply.github.com>
2025-11-15 12:16:39 +0530

    add instructions for player personality survey

--- src/User.java
+++ src/User.java
@@ -1,14 +1,19 @@
 public class User {
+    private String ID;
     private String name;
     private String email;
-    private String password;
-    private String role;
 
-    public User(String name, String email, String password, String role) {
+    public User(String ID, String name, String email) {
+        this.ID = ID;
         this.name = name;
         this.email = email;
-        this.password = password;
-        this.role = role;
+    }
+
+    public String getID() {
+        return ID;
+    }
+    public void setID(String ID) {
+        this.ID = ID;
     }
 
     public String getName() {
@@ -23,13 +28,5 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getRole() {
-        return role;
-    }
+
 }
